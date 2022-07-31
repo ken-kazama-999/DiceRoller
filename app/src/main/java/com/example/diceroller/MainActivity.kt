062f51369d3da562f9ec08f2758de00315057965
kazama-ken <kenkazama204@gmail.com>
2022-07-31 09:03:28 +0100

    Added Image For Dice Result

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 /**
@@ -16,6 +16,9 @@ class MainActivity : AppCompatActivity() {
 
         //Roll the dice when user presses button
         rollButton.setOnClickListener { rollDice() }
+
+        //Roll a dice when app starts
+        rollDice()
     }
 
     /**
@@ -25,10 +28,22 @@ class MainActivity : AppCompatActivity() {
         // create new dice objects having 6 sides and roll it.
         val dice = Dice(6)
         val diceRoll = dice.roll()
+        //Find the Imsge view in the app layout
+        val diceImage: ImageView = findViewById(R.id.imageView)
 
-        // Update the text on the screen with the dice roll.
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+        //Assigns the random result to variable "drawable Resource",and shows an image depending on the number of "dice roll"
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        //Displays the gotten Image as result in the app
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription = diceRoll.toString()
     }
 }
 
